f4831162690031d11dfe25022a2dd08ff46c3729
vegascruz <cruzv@students.westerntc.edu>
2023-10-09 21:25:19 -0500

    added components to the client side

--- server/server.js
+++ server/server.js
@@ -1,30 +1,17 @@
-// const express = require("express");
-// const bodyParser = require('body-parser');
-// const app = express();
-// const cors = require("cors");
-
-// const port = 5000;
-
-// app.use(bodyParser.urlencoded({extended: true}))
-// app.use(cors());
-// app.use(bodyParser.json())
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World');
-// })
-
-// app.listen(port, ()=> console.log('Server running on port 5000'));
-
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
+
 // get driver connection
 const dbo = require("./db/conn");
+
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
